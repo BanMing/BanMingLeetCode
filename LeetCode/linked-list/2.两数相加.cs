ceb7146756d816b8613f1ef055f3e121b5539f24
BanMing <banming729@163.com>
2019-05-30 00:41:12 +0800

    添加LeetCode插件

---
+++ LeetCode/linked-list/2.两数相加.cs
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=csharp
+ *
+ * [2] 两数相加
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        
+    // }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BanMingLeetCode
+niuke网上题
+
+LeetCode使用 vscode插件自动编写
