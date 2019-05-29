472982e6680a724ec6ef85208c4bdf16e3aedee0
BanMing <banming729@163.com>
2019-05-29 23:42:58 +0800

    maze

---
+++ CS/Maze/Maze.cs
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//// Maze.cs
+//// time:5/29/2019, 12:00:47 AM	
+//// author:BanMing	   
+//// des:https://www.nowcoder.com/practice/e3fc4f8094964a589735d640424b6a47?tpId=90&tqId=30779&tPage=1&rp=1&ru=/ta/2018test&qru=/ta/2018test/question-ranking
+////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Maze
+{
+
+
+
+    public void Func(int verLen, int horLen, List<List<int>> maze)
+    {
+        var res = 0;
+        for (int i = 0; i < verLen; i++)
+        {
+            for (int j = 0; j < horLen; j++)
+            {
+                if (maze[i][j]==2)
+                {
+                    
+                }
+            }
+        }
+    }
+
+}
