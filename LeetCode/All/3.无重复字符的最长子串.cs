183ec3c8abe23b079832655874242724d33e6d14
BanMing <banming729@163.com>
2019-06-06 09:20:37 +0800

    fixed

--- LeetCode/All/3.无重复字符的最长子串.cs
+++ LeetCode/All/3.无重复字符的最长子串.cs
@@ -5,8 +5,8 @@
  */
 using System;
 using System.Collections.Generic;
-// public class LengthOfLongestSubstringSolution
-public class Solution
+public class LengthOfLongestSubstringSolution
+// public class Solution
 
 {
     public int LengthOfLongestSubstringSelf(string s)
