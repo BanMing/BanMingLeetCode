7e46e62c0ee79be3d0e2246e0051ddc36d59b940
BanMing <banming729@163.com>
2019-06-12 00:39:24 +0800

    最大子序和

--- LeetCode/All/53.最大子序和.cs
+++ LeetCode/All/53.最大子序和.cs
@@ -3,8 +3,8 @@
  *
  * [53] 最大子序和
  */
-public class Solution
-// public class MaxSubArraySolution
+// public class Solution
+public class MaxSubArraySolution
 {
     public int MaxSubArray(int[] nums)
     {
@@ -14,40 +14,22 @@ public class Solution
         }
         int[] f = new int[nums.Length];
         f[0] = nums[0];
-        int max = int.MinValue;
+        int max = nums[0];
         for (int i = 1; i < nums.Length; i++)
         {
-            if (f[i - 1] < f[i - 1] + nums[i])
-            {
-                if (nums[i] > f[i - 1])
-                {
-                    f[i] = nums[i];
-                }
-                else
-                {
-                    if (f[i - 1] == f[i - 2])
-                    {
-
-                        f[i] = f[i - 1] + nums[i] + nums[i - 1];
-                    }
-                    else
-                    {
-                        f[i] = f[i - 1] + nums[i];
-                    }
-                }
-            }
-            else
-            {
-                f[i] = f[i - 1];
-            }
-            max = System.Math.Max(max, f[i]);
-            // System.Console.WriteLine($"f[{i}]={f[i]}");
+            f[i] = System.Math.Max(f[i - 1] + nums[i], nums[i]);
+            max = System.Math.Max(f[i], max);
+            System.Console.WriteLine($"f[{i}]={f[i]}");
         }
         return max;
     }
     public void Test()
     {
-        System.Console.WriteLine("-2,1,-3,4,-1,2,1,-5,4:" + MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        // System.Console.WriteLine("-2,1,-3,4,-1,2,1,-5,4:" + MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        System.Console.WriteLine("-2,1,-3:" + MaxSubArray(new int[] { -2, 1, -3 }));
+        // System.Console.WriteLine("1,1,-2:" + MaxSubArray(new int[] { 1, 1, -2 }));
+        // System.Console.WriteLine("-2,-1:" + MaxSubArray(new int[] { -2, -1 }));
+        // System.Console.WriteLine("8,-19,5,-4,20:" + MaxSubArray(new int[] { 8, -19, 5, -4, 20 }));
     }
 }
 
