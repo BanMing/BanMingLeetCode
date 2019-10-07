994f7ed07ca0744b2146998cd21dd00a7103b1cb
BanMing <banming729@163.com>
2019-10-08 07:53:49 +0800

    优化算法

--- LeetCode/C#/All/9.回文数.cs
+++ LeetCode/C#/All/9.回文数.cs
@@ -6,16 +6,12 @@
 using System;
 using System.Collections.Generic;
 
-public class IsPalindromeSolution
-// public class Solution
+// public class IsPalindromeSolution
+public class Solution
 {
 
     public bool IsPalindrome(int x)
     {
-        List<int> nums = new List<int>();
-        List<int> weis = new List<int>();
-        nums.Add(0);
-        weis.Add(0);
         if (x < 0)
         {
             return false;
@@ -24,23 +20,45 @@ public class IsPalindromeSolution
         {
             return true;
         }
-        for (int i = 1; i < int.MaxValue; i++)
+        // for (int i = 1; i < int.MaxValue; i++)
+        // {
+        //     var num = x % (int)Math.Pow(10, i);
+        //     if (nums.Contains(num) && num == x)
+        //     {
+        //         break;
+        //     }
+        //     nums.Add(num);
+        //     var wei = (num - nums[i - 1]) / (int)Math.Pow(10, i - 1);
+        //     weis.Add(wei);
+        // }
+        // int res = 0;
+        // for (int i = 1; i < weis.Count; i++)
+        // {
+        //     res += (int)Math.Pow(10, weis.Count - i - 1) * weis[i];
+        // }
+        // return res == x;
+        List<int> nums = new List<int>();
+        // List<int> weis = new List<int>();
+        // nums.Add(0);
+        // weis.Add(x);
+        var left = x;
+        for (int i = 0; i < x; i++)
         {
-            var num = x % (int)Math.Pow(10, i);
-            if (nums.Contains(num) && num == x)
+            // var left = weis[i] - nums[i];
+            if (left == 0)
             {
                 break;
             }
+            var num = (left) % 10;
+            left = (left - num) / 10;
             nums.Add(num);
-            var wei = (num - nums[i - 1]) / (int)Math.Pow(10, i - 1);
-            weis.Add(wei);
         }
-        int res = 0;
-        for (int i = 1; i < weis.Count; i++)
+        double res = 0;
+        for (int i = 0; i < nums.Count; i++)
         {
-            res += (int)Math.Pow(10, weis.Count - i - 1) * weis[i];
+            res += nums[i] * Math.Pow(10, nums.Count - i - 1);
         }
-        return res == x;
+        return (int)res == x;
     }
     public void Test()
     {
@@ -49,9 +67,9 @@ public class IsPalindromeSolution
         //     Console.WriteLine("%:" + 121 % Math.Pow(10, i));
         //     // Console.WriteLine("/:" + 121 / (int)Math.Pow(10, i));
         // }
-        // Console.WriteLine("121:" + IsPalindrome(121));
-        // Console.WriteLine("-121:" + IsPalindrome(-121));
-        // Console.WriteLine("10:" + IsPalindrome(10));
+        Console.WriteLine("121:" + IsPalindrome(121));
+        Console.WriteLine("-121:" + IsPalindrome(-121));
+        Console.WriteLine("10:" + IsPalindrome(10));
         Console.WriteLine("123:" + IsPalindrome(123));
     }
 
--- LeetCode/C#/Program.cs
+++ LeetCode/C#/Program.cs
@@ -6,7 +6,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var obj = new ThreeSumClosestSolution();
+            var obj = new IsPalindromeSolution();
             obj.Test();
         }
     }
