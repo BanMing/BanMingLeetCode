6b4211d17a9fb9d075ca09570dde5c3a89cca15a
BanMing <banming729@163.com>
2019-10-08 19:23:36 +0800

    修改编译

---
+++ LeetCode/C#/All/20.有效的括号.cs
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=csharp
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+// public class Solution
+public class IsValidSolution
+{
+    public bool IsValid(string s)
+    {
+        return true;
+    }
+    public void Test()
+    {
+
+    }
+}
+// @lc code=end
+
--- LeetCode/C#/Program.cs
+++ LeetCode/C#/Program.cs
@@ -6,7 +6,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var obj = new IsPalindromeSolution();
+            var obj = new IsValidSolution();
             obj.Test();
         }
     }
