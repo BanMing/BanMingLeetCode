95c2e37aaf54a11b9b64fe6ce7019ee4e97f9054
BanMing <banming729@163.com>
2019-12-26 22:22:31 +0800

    fixed

--- Offer/RepeatNum.cs
+++ Offer/RepeatNum.cs
@@ -3,6 +3,9 @@
 // 数组中重复的数字
 namespace Offer
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
     public class RepeatNum
     {
         public static int Test(int[] array)
@@ -67,6 +70,43 @@ namespace Offer
             return false;
         }
 
+
+        public static int Qestion2(int[] array)
+        {
+            int startIndex = 0;
+            int targetNum = array.Length / 2;
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= targetNum)
+                {
+                    count++;
+                }
+            }
+            if (count > targetNum)
+            {
+                targetNum /= 2;
+            }
+            else
+            {
+                targetNum = (targetNum + array.Length) / 2;
+            }
+
+        }
+        private static int getNumCount(int[] array, int startIndex, int middleIndex, int endIndex)
+        {
+            int resCount = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= middleIndex)
+                {
+                    resCount++;
+                }
+            }
+
+            return resCount;
+        }
     }
 
 }
