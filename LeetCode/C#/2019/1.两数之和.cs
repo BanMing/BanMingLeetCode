0387e2ab14a5d77abb2af6de0f7490f6116586d5
BanMing <banming729@163.com>
2020-04-12 07:27:33 +0800

    修改位置

---
+++ LeetCode/C#/2019/1.两数之和.cs
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=csharp
+ *
+ * [1] 两数之和
+ */
+namespace In2019
+{
+    using System.Collections.Generic;
+    public class TwoSumSolution
+    // public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dict[target - nums[i]], i };
+                }
+
+                dict[nums[i]] = i;
+            }
+            return null;
+        }
+    }
+}
+
