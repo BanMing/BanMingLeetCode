2523159d8b51c3c38e22e3a842546895517c3dee
BanMing <banming729@163.com>
2020-05-21 19:12:11 +0800

    二维数组中的查找

--- Offer/C++/main.cpp
+++ Offer/C++/main.cpp
@@ -1,7 +1,7 @@
+#include "2020/FindNumberIn2DArray.cpp"
 #include <iostream>
-#include "2020/FindRepeatNumber.cpp"
 int main(int, char **)
 {
-    std::cout << "Hello, world!\n";
+    std::cout << "Hello, world00!\n";
     Run();
 }
