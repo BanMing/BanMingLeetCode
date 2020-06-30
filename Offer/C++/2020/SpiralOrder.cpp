ba971f72ceb5d9a95f8e4402c4e9b7ad33ff3072
BanMing <banming729@163.com>
2020-06-30 14:24:13 +0800

    剑指 Offer 29. 顺时针打印矩阵

--- Offer/C++/2020/SpiralOrder.cpp
+++ Offer/C++/2020/SpiralOrder.cpp
@@ -18,22 +18,31 @@ public:
     vector<int> spiralOrder(vector<vector<int>> &matrix)
     {
         vector<int> res;
+
+        if (matrix.empty())
+        {
+            return res;
+        }
+
         size_t lenI = matrix.size();
         size_t lenJ = matrix[0].size();
+        size_t totalCount = lenI * lenJ;
         int i = 0, j = 0;
         int minI = 0, minJ = 0;
-        while (i < lenI || j < lenJ)
+        while (res.size() < totalCount && minI < lenI && minJ < lenJ)
         {
-            if (i < lenI && j < lenJ)
+            i = minI, j = minJ;
+            // if (i == minI && j == minJ)
+            // {
+
+            // }
+            while (j < lenJ)
             {
-                while (j < lenJ)
-                {
-                    res.push_back(matrix[i][j]);
-                    j++;
-                }
-                minI++;
+                res.push_back(matrix[i][j]);
+                j++;
             }
-            else if (i < lenI && j == lenJ)
+            minI++;
+            if (i < lenI && j == lenJ && i + 1 < lenI)
             {
                 j--;
                 i++;
@@ -42,30 +51,30 @@ public:
                     res.push_back(matrix[i][j]);
                     i++;
                 }
+                lenJ--;
             }
-            else if (i == lenI && j < lenJ)
+            if (i == lenI && j == lenJ && j - 1 >= minJ)
             {
                 i--;
                 j--;
-                while (j >= 0)
+                while (j >= minJ)
                 {
                     res.push_back(matrix[i][j]);
                     j--;
                 }
+                lenI--;
             }
-            else if (i == lenI && j < 0)
+            if (i == lenI && j == minJ - 1 && i - 1 >= minI)
             {
                 i--;
                 j++;
-                while (j >= 0)
+                while (i >= minI)
                 {
                     res.push_back(matrix[i][j]);
                     i--;
                 }
+                minJ++;
             }
-            minJ++;
-            lenJ--;
-            lenI--;
         }
 
         return res;
@@ -74,7 +83,7 @@ public:
 
 void Run()
 {
-    vector<vector<int>> test = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> test = {{1, 2, 3, 11, 14}, {1, 2, 3, 4, 5}, {4, 5, 6, 12, 15}, {7, 8, 9, 13, 16}};
     Solution solution;
     solution.spiralOrder(test);
 }
