1647b953363d5bc2a2f5ca1f06fdfd229be881c1
Wu Peng_a <wupeng_a@virtuosgames.com>
2020-08-18 11:25:17 +0800

    add

---
+++ LeetCode/C++/30days/DetectCapitalUse.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3409/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+    }
+};
