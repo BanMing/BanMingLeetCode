e24878ea81b76d5f47696799e2453b6b44a147af
BanMing <ban-ming@foxmail.com>
2020-08-18 13:28:57 +0800

    add detctCaitalUse

--- LeetCode/C++/30days/DetectCapitalUse.cpp
+++ LeetCode/C++/30days/DetectCapitalUse.cpp
@@ -7,5 +7,32 @@ class Solution
 public:
     bool detectCapitalUse(string word)
     {
+        if (word.empty())
+        {
+            return false;
+        }
+        // 65-90
+        // cout << word[0] << (int)word[0] << endl;
+        int firstIndex = (int)word[0];
+        bool isFirstUpper = firstIndex >= 65 && firstIndex <= 90;
+        bool isLower = true;
+        bool isUpper = true;
+        for (size_t i = 1; i < word.size(); i++)
+        {
+            int index = (int)word[i];
+            isLower = isLower && index >= 97 && index <= 122;
+            isUpper = isUpper && index >= 65 && index <= 90;
+            // cout << word[i] << (int)word[i] << endl;
+        }
+
+        return (isFirstUpper && (isLower || isUpper)) || ((!isFirstUpper) && isLower);
     }
 };
+
+void Run()
+{
+    Solution solution;
+    cout << solution.detectCapitalUse("USA") << endl;
+    cout << solution.detectCapitalUse("FlaG") << endl;
+    cout << solution.detectCapitalUse("leetcode") << endl;
+}
