b95d9c42a58dcd6c4e8bd6c45cf8dcdeefac20e1
BanMing <ban-ming@foxmail.com>
2020-08-19 11:54:10 +0800

    add findMaxConsecutiveOnesfindMaxConsecutiveOnes

--- LeetCode/C++/Array/FindMaxConsecutiveOnes.cpp
+++ LeetCode/C++/Array/FindMaxConsecutiveOnes.cpp
@@ -1,5 +1,7 @@
 
 
+// https://leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3238/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,18 +10,18 @@ class Solution
 public:
     int findMaxConsecutiveOnes(vector<int> &nums)
     {
-        int maxConsecutiveOnes = 1;
-        int currentCount = 1;
-        for (size_t i = 1; i < nums.size(); i++)
+        int maxConsecutiveOnes = 0;
+        int currentCount = 0;
+        for (size_t i = 0; i < nums.size(); i++)
         {
-            if (nums[i] == nums[i - 1])
+            if (nums[i] == 1)
             {
                 currentCount++;
             }
             else
             {
                 maxConsecutiveOnes = maxConsecutiveOnes < currentCount ? currentCount : maxConsecutiveOnes;
-                currentCount = 1;
+                currentCount = 0;
             }
         }
         maxConsecutiveOnes = maxConsecutiveOnes < currentCount ? currentCount : maxConsecutiveOnes;
@@ -32,3 +34,7 @@ void Run()
     Solution solution;
     cout << (solution.findMaxConsecutiveOnes(vector<int>{1, 1, 0, 1, 1, 1})) << endl;
 }
+
+#pragma region 讲解
+// 这个还是英文不好，看成最大连续数
+#pragma endregion
